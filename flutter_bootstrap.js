58f4abdec1a7ab41168847e8dbea0fee354dfa44
Your Name <you@example.com>
2026-08-14 09:04:45 +0530

    Disable service worker to fix caching issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,8 +35,4 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c0010","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "2833924071" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
-  }
-});
+_flutter.loader.load();
