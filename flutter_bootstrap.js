fd250bcba0ebdd4e6517f00409d9bd28f6e31cf8
Your Name <you@example.com>
2026-09-01 03:36:45 +0530

    Replace Independence Day banner with ONECONECT brand banner

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "874710773" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4104888299" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
